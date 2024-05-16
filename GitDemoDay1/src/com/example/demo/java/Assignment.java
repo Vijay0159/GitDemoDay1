25cfa0ae589b0fffa7e9cf9e0399c9705883291c
HP <HP@Vijay>
2024-05-16 13:44:40 +0530

    Update print method and add add method in Assignment class

--- GitDemoDay1/src/com/example/demo/java/Assignment.java
+++ GitDemoDay1/src/com/example/demo/java/Assignment.java
@@ -1,6 +1,10 @@
 package com.example.demo.java;
 public class Assignment {
     public String print() {
-        return "This is a message from Test branch.";
+        return "This is a message from TestChild branch.";
+    }
+
+    public int add(int a, int b) {
+        return a + b;
     }
 }
