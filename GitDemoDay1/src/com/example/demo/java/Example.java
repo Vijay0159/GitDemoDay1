44d409a187cbc2e0d88f2b65b680dc79f38df018
HP <HP@Vijay>
2024-05-16 15:29:36 +0530

    Add Example class with sample method.

--- GitDemoDay1/src/com/example/demo/java/Example.java
+++ GitDemoDay1/src/com/example/demo/java/Example.java
@@ -2,6 +2,6 @@ package com.example.demo.java;
 
 public class Example {
 	public void sampleMethod() {
-        System.out.println("This is a sample method.");
+        System.out.println("This is a sample method in example class.");
     }
 }
